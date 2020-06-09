b5a281f5948e3c858f7c134d0b75815d8708ad1e
Bani Diego <banidiego@gmail.com>
2020-06-09 14:13:45 +0200

    subida a Heroku

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -3,6 +3,8 @@ import { Request, Response } from 'express';
 class IndexController {
   public index(req: Request, res: Response) {
     res.json({ text: 'API is in /api/games' });
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    console.log(fullUrl);
   }
 }
 
