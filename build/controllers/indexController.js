8346b0027beef4c354bfcb7ca61500410ffeb122
Bani Diego <banidiego@gmail.com>
2020-06-09 14:15:12 +0200

    subida a Heroku

--- build/controllers/indexController.js
+++ build/controllers/indexController.js
@@ -1,11 +1,14 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.indexController = void 0;
 class IndexController {
-    index(req, res) {
-        res.json({ text: 'API is in /api/games' });
-        var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-        console.log(fullUrl);
-    }
+  index(req, res) {
+    res.json({ text: 'API is in /api/games' });
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    console.log(fullUrl);
+
+    // "build": "tsc -w",
+    // "dev": "nodemon build/index.js",
+  }
 }
 exports.indexController = new IndexController();
