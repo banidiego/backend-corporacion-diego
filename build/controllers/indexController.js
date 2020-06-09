fb7242611302495c6ed81f3875f96965d4fbe227
Bani Diego <banidiego@gmail.com>
2020-06-09 14:21:27 +0200

    subida a Heroku

--- build/controllers/indexController.js
+++ build/controllers/indexController.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.indexController = void 0;
 class IndexController {
     index(req, res) {
-        res.json({ text: 'API is in /api/games' });
+        // res.json({ text: 'API is in /api/games' });
         var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
         var port = req.app.settings.port;
         console.log(fullUrl);
--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 class IndexController {
   public index(req: Request, res: Response) {
-    res.json({ text: 'API is in /api/games' });
+    // res.json({ text: 'API is in /api/games' });
     var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
     var port = req.app.settings.port;
     console.log(fullUrl);
